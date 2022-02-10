232e13e4809196cd7c8ff5926731b3cbe3998c99
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-11 01:54:45 +0200

    Updates

---
+++ precache-manifest.1f440f3ce5ed7e5b013004acb8224df0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2df4b22183053528ce4a3554e4d7e7",
+    "url": "/menu-project/index.html"
+  },
+  {
+    "revision": "18dba20249bc013c6faa",
+    "url": "/menu-project/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/menu-project/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menu-project/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18dba20249bc013c6faa",
+    "url": "/menu-project/static/js/main.bb43dd31.chunk.js"
+  },
+  {
+    "revision": "019799a3c9d36b40ccba",
+    "url": "/menu-project/static/js/runtime-main.e93d96d8.js"
+  }
+]);
